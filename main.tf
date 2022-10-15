200024e6bf89c4a3501acdadca21b8a1a3124e18
Tim <tim@acme.org>
2022-10-15 17:20:20 +0200

    chore: Change AWS region to Europe Frankfurt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                      = "us-east-1"
+  region                      = "eu-central-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   access_key                  = "mock_access_key"
