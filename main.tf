8f71a10272a31408318f078e12cb59d3488d4a5c
Hugo <hugorut@gmail.com>
2024-11-28 16:41:43 +0100

    feat: add new web app

--- main.tf
+++ main.tf
@@ -29,3 +29,19 @@ resource "aws_lambda_function" "hello_world" {
   runtime       = "nodejs12.x"
   memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
 }
+
+resource "aws_instance" "web_app_2" {
+  ami           = "ami-674cbc1e"
+  instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  ebs_block_device {
+    device_name = "my_data"
+    volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
+    volume_size = 500
+    iops        = 800
+  }
+}
